14908042ae39820d833e57280e2ea5b004e2db2c
Scott Wahl <scott@eudaetics.com>
2026-07-17 10:03:36 -0600

    remove unnecessary Resolver class after previous refactor

--- stratae/depends/__init__.py
+++ stratae/depends/__init__.py
@@ -3,10 +3,8 @@
 from stratae.depends.depends import Depends, DependsWrapper
 from stratae.depends.inject import Injected, inject
 from stratae.depends.override import override, overrides
-from stratae.depends.resolver import Resolver
 
 __all__ = [
-    "Resolver",
     "Depends",
     "DependsWrapper",
     "override",
